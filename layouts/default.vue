252fdfc1996ed8a1a19ee4e0e6163d7d6e7b9883
fen1x <golian655@gmail.com>
2022-01-14 00:11:06 +0100

    Added custom scrollbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,3 +7,19 @@
   </div>
 </template>
 
+<style>
+::-webkit-scrollbar {
+  width: 16px;
+}
+
+::-webkit-scrollbar-thumb {
+  --tw-gradient-to: #00b3b7;
+  --tw-gradient-stops: var(--tw-gradient-from),
+  var(--tw-gradient-to, rgba(0, 179, 183, 0));
+  --tw-gradient-from: #0ce2af;
+  background-image: linear-gradient(to bottom, var(--tw-gradient-stops));
+  border-radius: 100px;
+}
+
+</style>
+
